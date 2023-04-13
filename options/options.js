a03628a8b2d15ee421d67713fa884c3a7db54898
Crowdbotics <team@crowdbotics.com>
2023-04-13 03:44:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "run-and-earn-39887",
+  url: "https://run-and-earn-39887.botics.co",
+  api: "https://run-and-earn-39887.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
